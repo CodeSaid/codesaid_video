16c991f04531666346088d751c4e77b5c44cedf5
CodeSaid <codesaid526@gmail.com>
2020-05-04 14:11:37 +0800

    NavGraphBuilder

---
+++ app/src/main/java/com/codesaid/utils/NavGraphBuilder.java
@@ -0,0 +1,60 @@
+package com.codesaid.utils;
+
+import android.content.ComponentName;
+
+import androidx.navigation.ActivityNavigator;
+import androidx.navigation.NavController;
+import androidx.navigation.NavGraph;
+import androidx.navigation.NavGraphNavigator;
+import androidx.navigation.NavigatorProvider;
+import androidx.navigation.fragment.FragmentNavigator;
+
+import com.codesaid.model.Destination;
+
+import java.util.HashMap;
+
+/**
+ * Created By codesaid
+ * On :2020-05-04 14:00
+ * Package Name: com.codesaid.utils
+ * desc:
+ */
+public class NavGraphBuilder {
+
+    public static void build(NavController controller) {
+        NavigatorProvider provider = controller.getNavigatorProvider();
+
+        FragmentNavigator fragmentNavigator = provider.getNavigator(FragmentNavigator.class);
+        ActivityNavigator activityNavigator = provider.getNavigator(ActivityNavigator.class);
+
+        NavGraph navGraph = new NavGraph(new NavGraphNavigator(provider));
+
+        HashMap<String, Destination> desConfig = AppConfig.getDesConfig();
+
+        for (Destination value : desConfig.values()) {
+            if (value.isFragment()) {
+                FragmentNavigator.Destination destination = fragmentNavigator.createDestination();
+                destination.setClassName(value.getClassName());
+                destination.setId(value.getId());
+                destination.addDeepLink(value.getPageUrl());
+
+                navGraph.addDestination(destination);
+            } else {
+                ActivityNavigator.Destination destination = activityNavigator.createDestination();
+                destination.setId(value.getId());
+                destination.addDeepLink(value.getPageUrl());
+                destination.setComponentName(
+                        new ComponentName(AppGlobals.getApplication().getPackageName(), value.getClassName()));
+
+                navGraph.addDestination(destination);
+            }
+
+            // 判断是否是 默认启动页面
+            if (value.isAsStarter()) {
+                navGraph.setStartDestination(value.getId());
+            }
+        }
+
+        controller.setGraph(navGraph);
+    }
+}
