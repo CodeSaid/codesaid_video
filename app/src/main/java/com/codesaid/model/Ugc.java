b8f2af18029715aa835a05af1e9e494a4cc8f440
CodeSaid <codesaid526@gmail.com>
2020-05-06 15:49:21 +0800

    首页 bean

---
+++ app/src/main/java/com/codesaid/model/Ugc.java
@@ -0,0 +1,86 @@
+package com.codesaid.model;
+
+import java.io.Serializable;
+
+/**
+ * Created By codesaid
+ * On :2020-05-06 15:43
+ * Package Name: com.codesaid.model
+ * desc:
+ */
+class Ugc implements Serializable {
+
+    /**
+     * likeCount : 153
+     * shareCount : 0
+     * commentCount : 4454
+     * hasFavorite : false
+     * hasLiked : true
+     * hasdiss:false
+     * hasDissed:false
+     */
+
+    public int likeCount;
+    public int shareCount;
+    public int commentCount;
+    public boolean hasFavorite;
+    public boolean hasLiked;
+    public boolean hasdiss;
+    public boolean hasDissed;
+
+    public int getLikeCount() {
+        return likeCount;
+    }
+
+    public void setLikeCount(int likeCount) {
+        this.likeCount = likeCount;
+    }
+
+    public int getShareCount() {
+        return shareCount;
+    }
+
+    public void setShareCount(int shareCount) {
+        this.shareCount = shareCount;
+    }
+
+    public int getCommentCount() {
+        return commentCount;
+    }
+
+    public void setCommentCount(int commentCount) {
+        this.commentCount = commentCount;
+    }
+
+    public boolean isHasFavorite() {
+        return hasFavorite;
+    }
+
+    public void setHasFavorite(boolean hasFavorite) {
+        this.hasFavorite = hasFavorite;
+    }
+
+    public boolean isHasLiked() {
+        return hasLiked;
+    }
+
+    public void setHasLiked(boolean hasLiked) {
+        this.hasLiked = hasLiked;
+    }
+
+    public boolean isHasdiss() {
+        return hasdiss;
+    }
+
+    public void setHasdiss(boolean hasdiss) {
+        this.hasdiss = hasdiss;
+    }
+
+    public boolean isHasDissed() {
+        return hasDissed;
+    }
+
+    public void setHasDissed(boolean hasDissed) {
+        this.hasDissed = hasDissed;
+    }
+}
---
+++ app/src/main/java/com/codesaid/model/User.java
@@ -0,0 +1,186 @@
+package com.codesaid.model;
+
+import java.io.Serializable;
+
+/**
+ * Created By codesaid
+ * On :2020-05-06 15:41
+ * Package Name: com.codesaid.model
+ * desc:
+ */
+public class User implements Serializable {
+
+    /**
+     * id : 962
+     * userId : 3223400206308231
+     * name : 二师弟请随我来
+     * avatar : https://p3-dy.byteimg.com/img/p1056/8c50025c85244140910a513345ae7358~200x200.webp
+     * description :
+     * likeCount : 0
+     * topCommentCount : 0
+     * followCount : 0
+     * followerCount : 0
+     * qqOpenId : null
+     * expires_time : 0
+     * score : 0
+     * historyCount : 0
+     * commentCount : 0
+     * favoriteCount : 0
+     * feedCount : 0
+     * hasFollow : false
+     */
+
+    public int id;
+    public long userId;
+    public String name;
+    public String avatar;
+    public String description;
+    public int likeCount;
+    public int topCommentCount;
+    public int followCount;
+    public int followerCount;
+    public String qqOpenId;
+    public long expires_time;
+    public int score;
+    public int historyCount;
+    public int commentCount;
+    public int favoriteCount;
+    public int feedCount;
+    public boolean hasFollow;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getLikeCount() {
+        return likeCount;
+    }
+
+    public void setLikeCount(int likeCount) {
+        this.likeCount = likeCount;
+    }
+
+    public int getTopCommentCount() {
+        return topCommentCount;
+    }
+
+    public void setTopCommentCount(int topCommentCount) {
+        this.topCommentCount = topCommentCount;
+    }
+
+    public int getFollowCount() {
+        return followCount;
+    }
+
+    public void setFollowCount(int followCount) {
+        this.followCount = followCount;
+    }
+
+    public int getFollowerCount() {
+        return followerCount;
+    }
+
+    public void setFollowerCount(int followerCount) {
+        this.followerCount = followerCount;
+    }
+
+    public String getQqOpenId() {
+        return qqOpenId;
+    }
+
+    public void setQqOpenId(String qqOpenId) {
+        this.qqOpenId = qqOpenId;
+    }
+
+    public long getExpires_time() {
+        return expires_time;
+    }
+
+    public void setExpires_time(long expires_time) {
+        this.expires_time = expires_time;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public int getHistoryCount() {
+        return historyCount;
+    }
+
+    public void setHistoryCount(int historyCount) {
+        this.historyCount = historyCount;
+    }
+
+    public int getCommentCount() {
+        return commentCount;
+    }
+
+    public void setCommentCount(int commentCount) {
+        this.commentCount = commentCount;
+    }
+
+    public int getFavoriteCount() {
+        return favoriteCount;
+    }
+
+    public void setFavoriteCount(int favoriteCount) {
+        this.favoriteCount = favoriteCount;
+    }
+
+    public int getFeedCount() {
+        return feedCount;
+    }
+
+    public void setFeedCount(int feedCount) {
+        this.feedCount = feedCount;
+    }
+
+    public boolean isHasFollow() {
+        return hasFollow;
+    }
+
+    public void setHasFollow(boolean hasFollow) {
+        this.hasFollow = hasFollow;
+    }
+}
