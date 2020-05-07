212d6c211b41c8098ec9daa01143cd213efa1951
CodeSaid <codesaid526@gmail.com>
2020-05-07 15:28:23 +0800

    图片 布局 完成

--- app/src/main/java/com/codesaid/view/CustomImageView.java
+++ app/src/main/java/com/codesaid/view/CustomImageView.java
@@ -5,12 +5,17 @@ import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.widget.AppCompatImageView;
 import androidx.databinding.BindingAdapter;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.RequestBuilder;
 import com.bumptech.glide.load.resource.bitmap.CircleCrop;
+import com.bumptech.glide.request.target.SimpleTarget;
+import com.bumptech.glide.request.transition.Transition;
+import com.codesaid.lib_base.PixUtils;
 
 /**
  * Created By codesaid
@@ -50,5 +55,55 @@ public class CustomImageView extends AppCompatImageView {
         }
 
         builder.into(view);
+    }
+
+    public void bindData(String imageUrl, int widthPix,
+                         int heightPix, int marginLeft) {
+        bindData(imageUrl, widthPix, heightPix, marginLeft,
+                PixUtils.getScreenWidht(), PixUtils.getScreenHeight());
+    }
+
+    /**
+     * @param imageUrl   图片 url
+     * @param widthPix   宽度
+     * @param heightPix  高度
+     * @param marginLeft 左边距
+     * @param maxWidth   最大宽度
+     * @param maxHeight  最大高度
+     */
+    public void bindData(String imageUrl, int widthPix,
+                         int heightPix, int marginLeft,
+                         int maxWidth, int maxHeight) {
+        if (widthPix <= 0 || heightPix <= 0) {
+            Glide.with(this).load(imageUrl).into(new SimpleTarget<Drawable>() {
+                @Override
+                public void onResourceReady(@NonNull Drawable resource, @Nullable Transition<? super Drawable> transition) {
+                    int height = resource.getIntrinsicHeight();
+                    int width = resource.getIntrinsicWidth();
+                    setSize(width, height, marginLeft, maxWidth, maxHeight);
+
+                    setImageDrawable(resource);
+                }
+            });
+            return;
+        }
+
+        setSize(widthPix, heightPix, marginLeft, maxWidth, maxHeight);
+        setImageUrl(this, imageUrl, false);
+    }
+
+    private void setSize(int width, int height, int marginLeft, int maxWidth, int maxHeight) {
+        int finalWidth, finalHeight;
+        if (width > height) {
+            finalWidth = maxWidth;
+            finalHeight = (int) (height / (width * 1.0f / finalWidth));
+        } else {
+            finalHeight = maxHeight;
+            finalWidth = (int) (width / (height * 1.0f / finalHeight));
+        }
+
+        ViewGroup.MarginLayoutParams params = new ViewGroup.MarginLayoutParams(finalWidth, finalHeight);
+        params.leftMargin = height > width ? PixUtils.dp2pix(marginLeft) : 0;
+        setLayoutParams(params);
     }
 }
---
+++ lib_base/src/main/java/com/codesaid/lib_base/PixUtils.java
@@ -0,0 +1,27 @@
+package com.codesaid.lib_base;
+
+import android.util.DisplayMetrics;
+
+/**
+ * Created By codesaid
+ * On :2020-05-07 15:20
+ * Package Name: com.codesaid.lib_base
+ * desc:
+ */
+public class PixUtils {
+
+    public static int dp2pix(int dpValue) {
+        DisplayMetrics displayMetrics = AppGlobals.getApplication().getResources().getDisplayMetrics();
+        return (int) (displayMetrics.density * dpValue + 0.5f);
+    }
+
+    public static int getScreenWidht() {
+        DisplayMetrics displayMetrics = AppGlobals.getApplication().getResources().getDisplayMetrics();
+        return displayMetrics.widthPixels;
+    }
+
+    public static int getScreenHeight() {
+        DisplayMetrics displayMetrics = AppGlobals.getApplication().getResources().getDisplayMetrics();
+        return displayMetrics.heightPixels;
+    }
+}
