2e5a252a66c87a1c3c6664a9eebb0d737dc7a193
CodeSaid <codesaid526@gmail.com>
2020-05-11 02:26:15 +0800

    实现用户 给帖子 点赞功能

---
+++ app/src/main/java/com/codesaid/ui/InteractionPresenter.java
@@ -0,0 +1,99 @@
+package com.codesaid.ui;
+
+import androidx.lifecycle.LifecycleOwner;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.Observer;
+
+import com.alibaba.fastjson.JSONObject;
+import com.codesaid.lib_base.AppGlobals;
+import com.codesaid.lib_network.ApiResponse;
+import com.codesaid.lib_network.ApiService;
+import com.codesaid.lib_network.callback.JsonCallback;
+import com.codesaid.model.Feed;
+import com.codesaid.model.User;
+import com.codesaid.ui.login.UserManager;
+
+/**
+ * Created By codesaid
+ * On :2020-05-11 00:45
+ * Package Name: com.codesaid.ui
+ * desc:
+ */
+public class InteractionPresenter {
+
+    public static final String DATA_FROM_INTERACTION = "data_from_interaction";
+
+    private static final String URL_TOGGLE_FEED_LIK = "/ugc/toggleFeedLike";
+
+    private static final String URL_TOGGLE_FEED_DISS = "/ugc/dissFeed";
+
+    private static final String URL_SHARE = "/ugc/increaseShareCount";
+
+    private static final String URL_TOGGLE_COMMENT_LIKE = "/ugc/toggleCommentLike";
+
+    public static void toggleFeedLike(LifecycleOwner owner, Feed feed) {
+
+        if (!UserManager.getInstance().isLogin()) {
+            LiveData<User> loginLiveData = UserManager.getInstance().login(AppGlobals.getApplication());
+            loginLiveData.observe(owner, new Observer<User>() {
+                @Override
+                public void onChanged(User user) {
+                    if (user != null) {
+                        toggleFeedLikeInternal(feed);
+                    }
+                    loginLiveData.removeObserver(this);
+                }
+            });
+            return;
+        }
+        toggleFeedLikeInternal(feed);
+    }
+
+    private static void toggleFeedLikeInternal(Feed feed) {
+        ApiService.get(URL_TOGGLE_FEED_LIK)
+                .addParam("userId", UserManager.getInstance().getUserId())
+                .addParam("itemId", feed.itemId)
+                .execute(new JsonCallback<JSONObject>() {
+                    @Override
+                    public void onSuccess(ApiResponse<JSONObject> response) {
+                        if (response.body != null) {
+                            boolean hasLiked = response.body.getBoolean("hasLiked").booleanValue();
+                            feed.getUgc().setHasdiss(hasLiked);
+                        }
+                    }
+                });
+    }
+
+    public static void toggleFeedDiss(LifecycleOwner owner, Feed feed) {
+        if (!UserManager.getInstance().isLogin()) {
+            LiveData<User> loginLiveData = UserManager.getInstance().login(AppGlobals.getApplication());
+            loginLiveData.observe(owner, new Observer<User>() {
+                @Override
+                public void onChanged(User user) {
+                    if (user != null) {
+                        toggleFeedDissInternal(feed);
+                    }
+                    loginLiveData.removeObserver(this);
+                }
+            });
+            return;
+        }
+        toggleFeedDissInternal(feed);
+    }
+
+    private static void toggleFeedDissInternal(Feed feed) {
+        ApiService.get(URL_TOGGLE_FEED_DISS)
+                .addParam("userId", UserManager.getInstance().getUserId())
+                .addParam("itemId", feed.itemId)
+                .execute(new JsonCallback<JSONObject>() {
+                    @Override
+                    public void onSuccess(ApiResponse<JSONObject> response) {
+                        if (response.body != null) {
+                            boolean hasLiked = response.body.getBoolean("hasLiked").booleanValue();
+                            feed.getUgc().setHasdiss(hasLiked);
+
+                        }
+                    }
+                });
+    }
+}
