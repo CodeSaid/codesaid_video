6cbfb42f3ccad070934c1cba5c6936876cf94058
CodeSaid <codesaid526@gmail.com>
2020-05-18 14:22:05 +0800

    帖子详情页 用户评论 数据展示

--- app/src/main/java/com/codesaid/ui/detail/FeedCommentAdapter.java
+++ app/src/main/java/com/codesaid/ui/detail/FeedCommentAdapter.java
@@ -1,5 +1,7 @@
 package com.codesaid.ui.detail;
 
+import android.text.TextUtils;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -7,8 +9,11 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.DiffUtil;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.codesaid.databinding.LayoutFeedCommentListItemBinding;
 import com.codesaid.lib_base.extention.AbsPagedListAdapter;
+import com.codesaid.lib_base.util.PixUtils;
 import com.codesaid.model.Comment;
+import com.codesaid.ui.login.UserManager;
 
 /**
  * Created By codesaid
@@ -40,17 +45,51 @@ public class FeedCommentAdapter extends AbsPagedListAdapter<Comment, FeedComment
 
     @Override
     protected ViewHolder onCreateViewHolder2(ViewGroup parent, int viewType) {
-        return null;
+        LayoutFeedCommentListItemBinding binding =
+                LayoutFeedCommentListItemBinding.inflate(
+                        LayoutInflater.from(parent.getContext()), parent, false);
+
+        return new ViewHolder(binding.getRoot(), binding);
     }
 
     @Override
     protected void onBindViewHolder2(ViewHolder holder, int position) {
-
+        Comment item = getItem(position);
+        holder.bindData(item);
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
-        public ViewHolder(@NonNull View itemView) {
+
+        private LayoutFeedCommentListItemBinding mBinding;
+
+        public ViewHolder(View itemView, LayoutFeedCommentListItemBinding binding) {
             super(itemView);
+            mBinding = binding;
+        }
+
+        public void bindData(Comment item) {
+            mBinding.setComment(item);
+            mBinding.labelAuthor.setVisibility(UserManager.getInstance().getUserId()
+                    == item.author.userId ? View.VISIBLE : View.GONE);
+            mBinding.commentDelete.setVisibility(UserManager.getInstance().getUserId()
+                    == item.author.userId ? View.VISIBLE : View.GONE);
+            if (!TextUtils.isEmpty(item.imageUrl)) {
+                mBinding.commentCover.setVisibility(View.VISIBLE);
+                mBinding.commentCover.bindData(item.width,
+                        item.height,
+                        0,
+                        PixUtils.dp2pix(200),
+                        PixUtils.dp2pix(200),
+                        item.imageUrl);
+                if (!TextUtils.isEmpty(item.videoUrl)) {
+                    mBinding.videoIcon.setVisibility(View.VISIBLE);
+                } else {
+                    mBinding.videoIcon.setVisibility(View.GONE);
+                }
+            } else {
+                mBinding.commentCover.setVisibility(View.GONE);
+                mBinding.videoIcon.setVisibility(View.GONE);
+            }
         }
     }
 }
